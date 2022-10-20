9c15e4bacd42ada31d793df3fd9ffa419b4e4b0e
Ata Tunç Bilge <96705530+redperson2003@users.noreply.github.com>
2022-10-20 19:26:16 +0300

    Add files via upload

---
+++ Homework1/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsmi giriniz : ");
+            String ad = Console.ReadLine();
+            for (int i = 0; i < ad.Length; i++) {Console.WriteLine(ad);}
+            String s = Console.ReadLine();
+
+
+        }
+    }
+    }
